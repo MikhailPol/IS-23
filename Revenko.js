9d6a9320dad0db026c824109b89efbd3ed4cea0e
Your Name <you@example.com>
2026-03-03 14:44:42 +0300

    revenko

--- Revenko.js
+++ Revenko.js
@@ -1,10 +1,13 @@
 function numb1(a,b) {
     return a+b
 };
+// function direction
 const num2 = function name(d,c) {
     return d+c
 };
+// function expretion
 const num3 = (n,j) => n+c;
+//стрелочная функция
 numb1(2,3);
 num2(1,3);
 num3(2,9);
