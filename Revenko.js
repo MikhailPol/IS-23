c41d29df3602c4fb026c45f98ec6e35c7f26b2c8
Your Name <you@example.com>
2026-03-13 11:59:32 +0300

    2 функции (Ревенко )

--- Revenko.js
+++ Revenko.js
@@ -1,13 +1,29 @@
-function numb1(a,b) {
-    return a+b
-};
-// function direction
-const num2 = function name(d,c) {
-    return d+c
-};
-// function expretion
-const num3 = (n,j) => n+c;
-//стрелочная функция
-numb1(2,3);
-num2(1,3);
-num3(2,9);
+// Функция, которая принимает на вход строку, и должна вернуть эту строку в ВЕРХНЕМ РЕГИСТРЕ. Если передана не строка или в ней есть цифра - ничего не должно происходить.
+// Функция, принимающая на вход 2 и возвращает сумму этих чисел. Если хотя бы одно не число - ничего не должно возвращаться.
+// Следите за названием функций и переменных, и проверяйте результат выполнения своего кода перед отправкой.
+// // Все изменения делайте в своих ветках.
+
+// задание 1
+function UP(str) {
+  if (typeof str !== "string") {
+    return str;
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(parseInt(str[i])) && str[i] !== " ") {
+      return str;
+    }
+
+    return str.toUpperCase();
+  }
+}
+console.log(UP("dfdfdfd"));
+console.log(UP[1234]);
+// задание2
+function sum(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return;
+  }
+  return a + b;
+}
+console.log(sum(2, "r"));
+console.log(sum(10, 11));
