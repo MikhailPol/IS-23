312118c3ce103824e9ffaf046b07cd0e79e9284d
Kirill <none@mail.ru>
2026-03-13 15:35:38 +0300

    Убрал alert

--- danilovich.js
+++ danilovich.js
@@ -1,12 +1,13 @@
-function alert_messege (messege) {
+function UpperCase_messege (messege) {
+    let text = messege
     if (typeof messege === "string" && isNaN(messege)) {
-        alert(messege)
+        console.log(text.toUpperCase(messege))
     }
     else {
         console.log("error")
     }
 }
-alert_messege("1")
+UpperCase_messege("Hello")
 
 function sum_of_numbers (num1, num2) {
    const numbers = (data) => {
